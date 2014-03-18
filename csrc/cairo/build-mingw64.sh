23c4afbdcf546a2f07053dcce63ea8217b2b5341
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2014-03-18 11:27:43 +0400

    mingw64 binaries

---
+++ csrc/cairo/build-mingw64.sh
@@ -0,0 +1,13 @@
+CC=gcc \
+PLATFORM=mingw64 \
+LIBNAME=cairo.dll \
+CFLAGS="-Wl,--enable-stdcall-fixup" \
+IMAGE_SURFACE=1 \
+PNG_FUNCTIONS=1 \
+RECORDING_SURFACE=1 \
+SVG_SURFACE=1 \
+PS_SURFACE=1 \
+PDF_SURFACE=1 \
+FT_FONT=1 \
+WIN32_SURFACE=1 \
+./build.sh
